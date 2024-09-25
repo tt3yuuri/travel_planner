2f9b6ebce1b909254075fc9ea2ee34d8f4d18c0e
junakimichi <michi.riku08@au.com>
2024-09-25 18:38:41 +0900

    TravelPlannerTest

--- Hosaka/Trip_planner_test.py
+++ Hosaka/Trip_planner_test.py
@@ -1,50 +1,7 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 import pandas as pd
-
-def redirect():
-    st.markdown("""
-        <meta http-equiv="refresh" content="0; url=https://www.google.com">
-        <p>リダイレクトしています...</p>
-    """, unsafe_allow_html=True)
-
-
-# 緯度と経度を設定
-latitude = -35.681236 # 例として東京駅の緯度
-longitude = 139.767125 # 例として東京駅の経度
-
-# 緯度と経度から地図用のデータフレームを作成
-data = pd.DataFrame()
-
-# 地図を表示
-def Chat(llm):
-
-    # チャット履歴の初期化
-        if "messages" not in st.session_state:
-            st.session_state.messages = [
-                SystemMessage(content="You are a trip planner and can provide you with great trip plans.")
-        ]
-
-        # ユーザーの入力を監視
-        if user_input := st.chat_input("聞きたいことを入力して下さい"):
-            st.session_state.messages.append(HumanMessage(content=user_input))
-            with st.spinner("ChatGPT is typing ..."):
-                response = st.llm(st.session_state.messages)
-            st.session_state.messages.append(AIMessage(content=response.content))
-
-        # チャット履歴の表示
-        messages = st.session_state.get('messages', [])
-        for message in messages:
-            if isinstance(message, AIMessage):
-                with st.chat_message('assistant'):
-                    st.markdown(message.content)
-            elif isinstance(message, HumanMessage):
-                with st.chat_message('user'):
-                    st.markdown(message.content)
-            #else:  # isinstance(message, SystemMessage):
-                #st.write(f"System message: {message.content}")
-
 
 def main():
     llm = ChatOpenAI(temperature=0)
@@ -54,74 +11,86 @@ def main():
         page_icon="🧳"
     )
     st.header("Trip Planner")
-    st.text("・このサイトは、皆さんのバカンスを最高なものにするために開発されました。")
-    st.text("・まずは目的地.グルメ.観光地などの気になる条件から入力してみましょう！")
-
+    st.text("・This site was developed to help you make the most of your vacation.")
+    st.text("・First, enter the conditions you are interested in, such as your destination, gourmet food, tourist spots, etc.")
+    
     # Sidebarの選択肢を定義する
-    
-    
-# Sidebarの選択肢を定義する
     options = ["START","MAP", "MEMO", "EXIT"]
     choice = st.sidebar.selectbox("Select an option", options)
-    budget = st.sidebar.number_input("予算（円）", min_value=1000, max_value=100000000, value=10000, step=1000)
-    date = st.date_input("Pick a date")
-    days = st.radio(
-        'How many days will you stay?', 
-        ['1', '2', '3', '4', '5',]
-    )
-    traffic = st.radio(
-        'Which transportation', 
-        ['飛行機', '船', '新幹線', 'タクシー', 'レンタカー','マイカー']
-    )
-    num_of_people = st.radio(
-        'How many people?', 
-        ['1', '2', '3', '4', '5',]
-    )
-    # Mainコンテンツの表示を変える
 
-    if  choice == "START":
+    if choice == "START":
         st.write("You selected START")
-        
-        st.write("滞在日数:",days)
-        st.write("人数:",num_of_people)
-        st.write("予算:",budget)
-        st.write("交通機関:",traffic)
-        st.write("日付:",date)
-        if st.button('Search for this content'):
-            search_contents = ("滞在日数は",days,"日,",
-                              "人数は",num_of_people,"人,","予算は",budget,"円,",
-                              "交通機関は",traffic,"の旅行プランを計画してください")
-            
-            if "messages" not in st.session_state:
-                st.session_state.messages = [
-                SystemMessage(content="You are a trip planner and can provide you with great trip plans.")
-        ]
-            llm = ChatOpenAI(temperature=0)
-            st.session_state.messages.append(HumanMessage(content=search_contents))
-            with st.spinner("ChatGPT is typing ..."):
-                response = st.llm(st.session_state.messages)
-            st.session_state.messages.append(AIMessage(content=response.content))
-
-            messages = st.session_state.get('messages', [])
-
-            for message in messages:
-                if isinstance(message, AIMessage):
-                    with st.chat_message('assistant'):
-                        st.markdown(message.content)
-                elif isinstance(message, HumanMessage):
-                    with st.chat_message('user'):
-                        st.markdown(message.content)
-
+        AI()
+        condition()
     elif choice == "MAP":
         st.write("You selected MAP")
-        st.map(data)
+        MAP()
     elif choice == "MEMO":
         st.write("You selected MEMO")
-        Chat()
+        AI()
     else:
         st.write("You selected EXIT")
         redirect()
+
+def redirect():
+    st.markdown("""
+        <meta http-equiv="refresh" content="0; url=https://www.google.com">
+        <p>リダイレクトしています...</p>
+    """, unsafe_allow_html=True)
+
+def MAP():
+    # 緯度と経度を設定
+    latitude = 55
+    longitude = -3
+    data = pd.DataFrame({'lat': [latitude], 'lon': [longitude]})
+    st.map(data)
+
+def AI():
+    llm = ChatOpenAI(temperature=0)
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a trip planner. You should provide great trip plans.")
+        ]
+
+    if user_input := st.chat_input("質問を入力してください:"):
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("ChatGPT is typing ..."):
+            response = llm(st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=response.content))
+
+    messages = st.session_state.get('messages', [])
+    for message in messages:
+        if isinstance(message, AIMessage):
+            st.markdown(f"**Assistant:** {message.content}")
+        elif isinstance(message, HumanMessage):
+            st.markdown(f"**You:** {message.content}")
+
+def condition():
+    #date = st.date_input("Pick a date")
+    days = st.slider('宿泊日数', 1, 14, 1)
+    people = st.radio('人数', ['1人', '2人', '3人', '4人', 'それ以上'])
+    traffic = st.radio('交通', ["飛行機", "船", "新幹線", "タクシー", "レンタカー", "自家用車"])
+    cost = st.sidebar.number_input("予算（円）", min_value=1000, max_value=100000000, value=10000, step=1000)
+    if st.button("検索する"):
+        sentence = f"滞在日数は{days}日, 人数は{people}, 予算は{cost}円, 交通機関は{traffic}の旅行プランを計画してください。"
+        question(sentence)
+
+def question(sentence):
+    llm = ChatOpenAI(temperature=0)
+
+    st.write("この条件で検索しています・・・")
     
+    st.session_state.messages.append(HumanMessage(content=sentence))
+    with st.spinner("ChatGPT is typing ..."):
+        response = llm(st.session_state.messages)
+    st.session_state.messages.append(AIMessage(content=response.content))
+
+    for message in st.session_state.messages:
+        if isinstance(message, AIMessage):
+            st.markdown(f"**Assistant:** {message.content}")
+        elif isinstance(message, HumanMessage):
+            st.markdown(f"**You:** {message.content}")
 
 if __name__ == '__main__':
     main()
