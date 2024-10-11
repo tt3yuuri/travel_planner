3f33fe2316b116576214880885098f05a2e008c0
junakimichi <michi.riku08@au.com>
2024-10-11 19:54:11 +0900

    コードを最新に変更

--- Hosaka/Trip_planner_test.py
+++ Hosaka/Trip_planner_test.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 import pandas as pd
-
+#master→main
 def main():
     llm = ChatOpenAI(temperature=0)
 
@@ -13,7 +13,7 @@ def main():
     st.header("Trip Planner")
     st.text("・This site was developed to help you make the most of your vacation.")
     st.text("・First, enter the conditions you are interested in,")
-    st.text("  such as your destination, gourmet food, tourist spots, etc.")
+    st.text("    such as your destination, gourmet food, tourist spots, etc.")
     # Sidebarの選択肢を定義する
     options = ["START","MAP", "MEMO", "EXIT"]
     choice = st.sidebar.selectbox("Select an option", options)
@@ -35,7 +35,7 @@ def main():
 def redirect():
     st.markdown("""
         <meta http-equiv="refresh" content="0; url=https://www.google.com">
-        <p>リダイレクトしています...</p>
+        <p>Redirecting...</p>
     """, unsafe_allow_html=True)
 
 def MAP():
@@ -53,10 +53,10 @@ def AI():
             SystemMessage(content="You are a trip planner. You should provide great trip plans.")
         ]
 
-    if user_input := st.chat_input("質問を入力してください:"):
+    if user_input := st.chat_input("Enter your question:"):
         st.session_state.messages.append(HumanMessage(content=user_input))
         with st.spinner("ChatGPT is typing ..."):
-            response = llm(st.session_state.messages)
+            response = llm.invoke(st.session_state.messages)
         st.session_state.messages.append(AIMessage(content=response.content))
 
     messages = st.session_state.get('messages', [])
@@ -67,13 +67,18 @@ def AI():
             st.markdown(f"**You:** {message.content}")
 
 def condition():
-    #date = st.date_input("Pick a date")
+    # 国を入力させるフィールドを追加
+    todofuken = ["北海道","東北地方","関東地方","中部地方","近畿地方","中国地方","四国地方","九州地方","沖縄県"]
+    region = st.selectbox("出発地",todofuken)
+    #country = st.text_input('行きたい国', '日本')  # デフォルトを「日本」に設定
     days = st.slider('宿泊日数', 1, 14, 1)
     people = st.radio('人数', ['1人', '2人', '3人', '4人', 'それ以上'])
     traffic = st.radio('交通', ["飛行機", "船", "新幹線", "タクシー", "レンタカー", "自家用車"])
     cost = st.sidebar.number_input("予算（円）", min_value=1000, max_value=100000000, value=10000, step=1000)
+
     if st.button("検索する"):
-        sentence = f"滞在日数は{days}日, 人数は{people}, 予算は{cost}円, 交通機関は{traffic}の旅行プランを計画してください。"
+        # 入力された国を検索条件に追加
+        sentence = f"行きたい地方は{region}, 滞在日数は{days}日, 人数は{people}, 予算は{cost}円, 交通機関は{traffic}の旅行プランを計画してください。"
         question(sentence)
 
 def question(sentence):
@@ -83,7 +88,7 @@ def question(sentence):
     
     st.session_state.messages.append(HumanMessage(content=sentence))
     with st.spinner("ChatGPT is typing ..."):
-        response = llm(st.session_state.messages)
+        response = llm.invoke(st.session_state.messages)
     st.session_state.messages.append(AIMessage(content=response.content))
 
     for message in st.session_state.messages:
@@ -91,6 +96,7 @@ def question(sentence):
             st.markdown(f"**Assistant:** {message.content}")
         elif isinstance(message, HumanMessage):
             st.markdown(f"**You:** {message.content}")
+
 
 if __name__ == '__main__':
     main()
