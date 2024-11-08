70cda150151eabe02355670f06412d1549cb03d5
tt3yuuri0617 <tt3yuuri@gmail.com>
2024-11-08 19:48:58 +0900

    duckduckgotest

--- duckduckgotest.py
+++ duckduckgotest.py
@@ -1,4 +1,5 @@
 from duckduckgo_search import DDGS
 
-results = DDGS().text("ドラフト会議2024 速報",region="jp-jp",max_results=3)
+Question = "リンゴ"
+results = DDGS().text(Question,region="jp-jp",max_results=3)
 print (results)
