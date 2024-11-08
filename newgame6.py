8bdabcc54c91fb5349b7f68d7b036454491c7e53
tt3yuuri0617 <tt3yuuri@gmail.com>
2024-11-08 20:24:47 +0900

    edit6

--- newgame6.py
+++ newgame6.py
@@ -11,7 +11,7 @@ import folium
 from duckduckgo_search import DDGS
 
 def main():
-    llm = ChatOpenAI(temperature=0)
+    #llm = ChatOpenAI(temperature=0)
 
     st.set_page_config(
         page_title="Trip Planner",
@@ -21,7 +21,7 @@ def main():
     st.text("・このサイトは、皆さんのバカンスを最高なものにするために開発されました。")
     st.text("・まずは目的地.グルメ.観光地などの気になる条件から入力してみましょう！")
     # Sidebarの選択肢を定義する
-    options = ["MEMO", "MAP", "EXIT"]
+    options = ["MEMO","DUCK", "MAP", "EXIT"]
     choice = st.sidebar.selectbox("Select an option", options)
     # Mainコンテンツの表示を変える
     if choice == "MAP":
@@ -31,6 +31,9 @@ def main():
         st.write("You selected MEMO")
         condition()
         AI()
+    elif choice == "DUCK":
+        st.write("You selected DUCK")
+        duckduckgo()
     else:
         st.write("You selected EXIT")
         redirect()
@@ -178,6 +181,42 @@ def question_response():
                 st.markdown(message.content)
         else:  # isinstance(message, SystemMessage):
             st.write(f"System message: {message.content}")
+    
+def duckduckgo():
+    st.title("duckduckgo 検索結果")
 
+    # 検索を実行する関数
+    def search_duckduckgo(query):
+        results = DDGS().text(query, region="jp-jp", max_results=33)
+        # 検索結果があるかどうかチェックする
+        if results:
+            # 検索結果の最初の項目のタイトルとURLを取得する
+            first_result = results[0]
+            title = first_result['title']
+            href = first_result['href']
+            # タイトルとURLを表示する
+            st.write(f"1: {title}")
+            st.write(f"URL: {href}")
+            # 検索結果の二番目の項目のタイトルとURLを取得する
+            second_result = results[1]
+            title2 = second_result['title']
+            href2 = second_result['href']
+            # タイトルとURLを表示する
+            st.write(f"2: {title2}")
+            st.write(f"URL: {href2}")
+            # 検索結果の三番目の項目のタイトルとURLを取得する
+            third_result = results[2]
+            title3 = third_result['title']
+            href3 = third_result['href']
+            # タイトルとURLを表示する
+            st.write(f"3: {title3}")
+            st.write(f"URL: {href3}")
+        else:
+            # 検索結果がなかった場合のメッセージを表示する
+            st.write("検索結果が見つかりませんでした。")
+
+    # 検索を実行する
+    Question = "リンゴ"
+    search_duckduckgo(Question)
 if __name__ == '__main__':
     main()
