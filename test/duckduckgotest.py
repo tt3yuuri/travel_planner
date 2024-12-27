1d7528394aa5514aa775ace526d566b03d1a5419
tt3yuuri0617 <tt3yuuri@gmail.com>
2024-12-27 19:55:14 +0900

    test

---
+++ test/duckduckgotest.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from duckduckgo_search import DDGS
+
+def duckduckgo():
+
+    st.title("duckduckgo 検索結果")
+
+    # 検索を実行する関数
+    def search_duckduckgo(query):
+        results = DDGS().text(query, region="jp-jp", max_results=33)
+        # 検索結果があるかどうかチェックする
+        if results:
+            # 検索結果の最初の項目のタイトルとURLを取得する
+            first_result = results[0]
+            title = first_result['title']
+            href = first_result['href']
+            # タイトルとURLを表示する
+            st.write(f"1: {title}")
+            st.write(f"URL: {href}")
+            # 検索結果の二番目の項目のタイトルとURLを取得する
+            second_result = results[1]
+            title2 = second_result['title']
+            href2 = second_result['href']
+            # タイトルとURLを表示する
+            st.write(f"2: {title2}")
+            st.write(f"URL: {href2}")
+            # 検索結果の三番目の項目のタイトルとURLを取得する
+            third_result = results[2]
+            title3 = third_result['title']
+            href3 = third_result['href']
+            # タイトルとURLを表示する
+            st.write(f"3: {title3}")
+            st.write(f"URL: {href3}")
+        else:
+            # 検索結果がなかった場合のメッセージを表示する
+            st.write("検索結果が見つかりませんでした。")
+
+    # 検索を実行する
+    Question = "リンゴ"
+    search_duckduckgo(Question)
+
+duckduckgo()
