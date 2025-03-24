0f058f44488518cf47c970f18ad3d9113428780e
tt3yuuri0617 <tt3yuuri@gmail.com>
2025-03-24 21:27:11 +0900

    edit

--- new.py
+++ new.py
@@ -10,7 +10,6 @@ from  streamlit_folium import st_folium
 import folium
 from duckduckgo_search import DDGS
 import datetime
-import pyautogui as pg
 
 def main():
     #llm = ChatOpenAI(temperature=0)
